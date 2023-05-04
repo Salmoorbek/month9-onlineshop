ecc060949b2e1408a47dd48dd278dce00e662375
Salmoor Nurzhanov <salmor662@gmail.com>
2023-05-04 14:28:51 +0600

    added classes Product, Order, User

---
+++ src/main/java/com/example/onlinestore/entity/Product.java
@@ -0,0 +1,24 @@
+package com.example.onlinestore.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "image_url")
+    private String image;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "quantity")
+    private int quantity;
+    @Column(name = "price")
+    private Double price;
+}
