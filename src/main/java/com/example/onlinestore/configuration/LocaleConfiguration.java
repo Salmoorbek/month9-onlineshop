cde1ccc7df9a5320d2c841a059b3a9f5f9a1eaad
Salmoor Nurzhanov <salmor662@gmail.com>
2023-05-29 18:56:05 +0600

    I tried to do 2 and 3 tasks out of 72 homework, but nothing happened

---
+++ src/main/java/com/example/onlinestore/configuration/LocaleConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.onlinestore.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class LocaleConfiguration implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver getLocalResolver() {
+        return new SessionLocaleResolver();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    private LocaleChangeInterceptor localeChangeInterceptor() {
+        var loc = new LocaleChangeInterceptor();
+        loc.setParamName("lang");
+        return loc;
+    }
+}
